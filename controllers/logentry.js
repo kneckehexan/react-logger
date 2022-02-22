c4b2fb836388e5918591cf198180801a533c89e2
Philip Tunbjer <philip.tunbjer@gmail.com>
2022-02-22 23:37:20 +0100

    DEV: Removed unused files
    Removed files pertaining to log and log entry models and controllers.

--- controllers/logentry.js
+++
@@ -1,8 +0,0 @@
-const Logentry = require('../models/LogEntry');
-const { StatusCodes } = require('http-status-codes');
-const {BadRequestError, NotFoundError} = require('../errors');
-
-const getAllLogEntries = async (req, res) => {
-  const {user: {userId}} = req;
-  const logentries = await Logentry.find({})
-}
